1b32641e7d76e58dced46f6f786ee1cc7110cd41
florian denis <136095872+Azherak@users.noreply.github.com>
2024-01-17 20:20:42 +0100

    fix js
    - debut fix next et previous fleche
    - :root classification;
    - hover cursor pointer;
    - seo titre
    - fix img /;
    - fix form;

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,7 +154,7 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index-1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
@@ -192,7 +192,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
